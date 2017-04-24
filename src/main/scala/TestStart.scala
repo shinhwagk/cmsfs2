bc5f85a6992eea42d6da27ce7a3f19e23c1964b2
gk <shanghai_sl@hotmail.com>
2017-04-24 15:22:33 +0800

    save

--- src/main/scala/TestStart.scala
+++ src/main/scala/TestStart.scala
@@ -2,10 +2,10 @@ import org.cmsfs.Startup
 
 object TestStart {
   def main(args: Array[String]): Unit = {
-    Startup.main(Seq("bootstrap", "2579", "aaaa:2579").toArray)
-    //    Thread.sleep(1000)
-    //    StartupCmsfs.main(Seq("collect-script-local", "2765").toArray)
-    //    Thread.sleep(1000)
-    Startup.main(Seq("format-script", "2769", "aaaa:2579").toArray)
+    Startup.main(Seq("bootstrap", "2579", "127.0.0.1:2579").toArray)
+    Thread.sleep(1000)
+    Startup.main(Seq("collect-ssh-script", "2765", "127.0.0.1:2579").toArray)
+    Thread.sleep(1000)
+    Startup.main(Seq("format-script", "2769", "127.0.0.1:2579").toArray)
   }
 }
